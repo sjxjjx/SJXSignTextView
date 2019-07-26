c6074710f2ef1183c478c4376008cc4fdccecfa1
宋佳鑫 <songjiaxin@gouuse.cn>
2019-07-26 22:55:04 +0800

    使用演示代码

--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "SJXSignTextView.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self demo_right];
+    
+    [self demo_left];
+    
+    [self demo_all];
 }
 
+- (void)demo_right {
+    UIView *v = [[UIView alloc] init];
+    v.frame = CGRectMake(50, 100, 100, 100);
+    v.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:v];
+    
+    SJXSignTextView *testV = [[SJXSignTextView alloc] init];
+    testV.frame = CGRectMake(60, 0, 40, 40);
+    testV.directionType = TextDirectionTypeRight;
+    testV.text = @"测试文字";
+    testV.textColor = [UIColor whiteColor];
+    testV.bgColor = [UIColor redColor];
+    testV.textFont = 8;
+    [v addSubview:testV];
+}
+
+- (void)demo_left {
+    UIView *v = [[UIView alloc] init];
+    v.frame = CGRectMake(200, 100, 100, 100);
+    v.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:v];
+    
+    SJXSignTextView *testV = [[SJXSignTextView alloc] init];
+    testV.frame = CGRectMake(0, 0, 40, 40);
+    testV.directionType = TextDirectionTypeLeft;
+    testV.text = @"测试文字";
+    testV.textColor = [UIColor whiteColor];
+    testV.bgColor = [UIColor blueColor];
+    testV.textFont = 8;
+    [v addSubview:testV];
+}
+
+- (void)demo_all {
+    UIView *v = [[UIView alloc] init];
+    v.frame = CGRectMake(100, 300, 100, 100);
+    v.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:v];
+    
+    SJXSignTextView *testV = [[SJXSignTextView alloc] init];
+    testV.frame = CGRectMake(0, 0, 40, 40);
+    testV.directionType = TextDirectionTypeLeft;
+    testV.text = @"测试文字";
+    testV.textColor = [UIColor redColor];
+    testV.bgColor = [UIColor whiteColor];
+    testV.textFont = 8;
+    [v addSubview:testV];
+    
+    SJXSignTextView *testV2 = [[SJXSignTextView alloc] init];
+    testV2.frame = CGRectMake(60, 0, 40, 40);
+    testV2.directionType = TextDirectionTypeRight;
+    testV2.text = @"测试文字";
+    testV2.textColor = [UIColor blackColor];
+    testV2.bgColor = [UIColor greenColor];
+    testV2.textFont = 8;
+    [v addSubview:testV2];
+}
 
 @end
